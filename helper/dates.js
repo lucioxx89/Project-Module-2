ca0e4ab03ff0c22905d1814c01f2463a0234ee4d
Ale Bausa <alebausa@macbook-pro-de-ale.home>
2021-05-13 23:45:16 +0200

    Adds comments to date helper

--- helper/dates.js
+++ helper/dates.js
@@ -1,4 +1,6 @@
 const fixDate = function(date) {
+    // Solo necesito que me enseñe los 16 primeros dígitos y se cargue los segundos y milisegundos
+    // Porque sino el edit no lo sabe leer
     return date.slice(0, 16).toString();
 }
 module.exports = fixDate;
