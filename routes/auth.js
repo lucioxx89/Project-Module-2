42bb65ce0dc75cce9f969b3f755d395075bf990f
andrea ilieschi <andrea.ilieschi@gmail.com>
2021-05-15 12:43:09 +0200

    commit a buffo

--- routes/auth.js
+++ routes/auth.js
@@ -33,4 +33,43 @@ router.get('/login', (req, res, next) => {
   res.render('auth/login');
 });
 
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+ 
+  if (email === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, email and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ email }) // <== check if there's user with the provided email
+    .then(user => {
+      // <== "user" here is just a placeholder and represents the response from the DB
+      if (!user) {
+        // <== if there's no user with provided email, notify the user who is trying to login
+        res.render('auth/login', {
+          errorMessage: 'Email is not registered. Try with other email.'
+        });
+        return;
+      }
+ 
+      // if there's a user, compare provided password
+      // with the hashed password saved in the database
+      else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        // if the two passwords match, render the userProfile.hbs and
+        //                   pass the user object to this view
+        //                                      |
+        //                                      V
+        res.render('users/userProfile', { user });
+      } else {
+        // if the two passwords DON'T match, render the login form again
+        // and send the error message to the user
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
+
 module.exports = router;
