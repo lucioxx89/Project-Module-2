7e8ae6049054b70d7cc289c88c3ba157833dc123
Luciano Colabraro <luciano.colabraro@gmail.com>
2021-05-15 12:20:30 +0200

    add login get route

--- routes/auth.js
+++ routes/auth.js
@@ -33,4 +33,28 @@ router.get('/login', (req, res, next) => {
   res.render('auth/login');
 });
 
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (email === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, email and password to login.',
+    });
+    return;
+  }
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+
+        return;
+      } else if (bcryptjs.compareSync(password, user.passworHash)) {
+        res.render('users/user-profile', { user });
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
 module.exports = router;
